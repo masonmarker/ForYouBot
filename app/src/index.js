8aa87aea3a2354fa6a543e980fd62ceb0d05a4d3
masonmarker <markermd@dukes.jmu.edu>
2023-02-18 21:19:55 -0500

    Bot Website Update

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Pages
 import App from './App';
+
+
+// Chakra UI
+import { ChakraProvider } from '@chakra-ui/react';
+
+
+// Routing
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </ChakraProvider>
 );
