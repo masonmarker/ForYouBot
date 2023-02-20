885cc9574574c0779008db70ec88517d8256bfe9
masonmarker <markermd@dukes.jmu.edu>
2023-02-20 15:18:41 -0500

    Bot Website Update

--- app/src/components/ColorButton.js
+++ app/src/components/ColorButton.js
@@ -12,17 +12,30 @@ import {
     useColorMode
 } from "@chakra-ui/react"
 
+// sun and moon icons
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
+// styled components
+import styled from 'styled-components'
 
-
+// styled ColorButton
+// top left corner
+const ColorButtonStyled = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin: 1rem;
+`
 
 // ColorButton component
 const ColorButton = () => {
     const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <Button onClick={toggleColorMode}>
-            Toggle {colorMode === "light" ? "Dark" : "Light"}
-        </Button>
+        <ColorButtonStyled>
+            <Button onClick={toggleColorMode} >
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+        </ColorButtonStyled>
     )
 }
 
