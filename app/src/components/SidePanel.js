ddeb6485abd04bcdc088b0431c2b051e5aaf70ab
HarrisChaud <chaudhhx@dukes.jmu.edu>
2023-02-25 20:44:48 -0500

    nodemon and express installed

--- app/src/components/SidePanel.js
+++ app/src/components/SidePanel.js
@@ -1,96 +1,95 @@
 /**
- * Side menu panel, capable of being placed 
+ * Side menu panel, capable of being placed
  * on any side of the screen
- * 
- * author : 
+ *
+ * author :
  *  Mason Marker
  */
 
 // React
-import React from "react"
+import React from "react";
 
 // styled components
-import styled from "styled-components"
+import styled from "styled-components";
 
 // Chakra components
 import {
-    RadioGroup,
-    Stack,
-    Radio,
-    Button,
-    Drawer,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerHeader,
-    DrawerBody,
-    DrawerCloseButton,
-    Text,
-    useDisclosure,
-    useColorMode,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-} from "@chakra-ui/react"
+  RadioGroup,
+  Stack,
+  Radio,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  DrawerCloseButton,
+  Text,
+  useDisclosure,
+  useColorMode,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
 // styled SidePanel
 // should be permanently in the bottom right corner
 const SidePanelStyled = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  /* bottom right corner */
+  .button {
     position: fixed;
     bottom: 0;
-    left: 0;
+    right: 0;
     margin: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-
-    /* bottom right corner */
-    .button { 
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        margin: 1rem;
-    }
-`
+  }
+`;
 
 // SidePanel component
 const SidePanel = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const finalRef = React.useRef(null)
+  const finalRef = React.useRef(null);
 
-
-    return (
-        <SidePanelStyled>
-            <Button className="button" colorScheme='purple' onClick={(e) => {
-                onOpen()
-                e.stopPropagation()
-            }}>
-                Edit Bot
+  return (
+    <SidePanelStyled>
+      <Button
+        className="button"
+        colorScheme="purple"
+        onClick={(e) => {
+          onOpen();
+          e.stopPropagation();
+        }}
+      >
+        Edit Bot
+      </Button>
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Bot</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Edit interface</ModalBody>
+          <ModalFooter>
+            <Button colorScheme="purple" mr={3} onClick={onClose}>
+              Close
             </Button>
-            <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Edit Bot</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        Hello
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='purple' mr={3} onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </SidePanelStyled>
-    )
-}
+            <Button variant="ghost">Customize Interface</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </SidePanelStyled>
+  );
+};
 
-
-
-export default SidePanel
+export default SidePanel;
