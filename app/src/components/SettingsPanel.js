672caaf088f6ab7ea56a796317a027b6843cc8dd
HarrisChaud <chaudhhx@dukes.jmu.edu>
2023-02-25 19:58:19 -0500

    goofy

--- app/src/components/SettingsPanel.js
+++ app/src/components/SettingsPanel.js
@@ -1,107 +1,110 @@
 /**
  * Settings Panel, includes both the button and the Drawer.
- * 
+ *
  * author :
  *  Mason Marker
  */
 
 // React
-import React from 'react'
+import React from "react";
 
 // Chakra components
 import {
-    Text,
-    Button,
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-
-} from "@chakra-ui/react"
+  Text,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
 // styled components
-import styled from 'styled-components'
+import styled from "styled-components";
 
 // styled SettingsPanel
-const SettingsPanelStyled = styled.div`
-
-`
+const SettingsPanelStyled = styled.div``;
 
 // SettingsPanel component
 const SettingsPanel = () => {
+  // modal ref
+  const finalRef = React.useRef(null);
 
-    // modal ref
-    const finalRef = React.useRef(null)
+  // drawer state
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
+  // help drawer state
+  const {
+    isOpen: isOpenHelp,
+    onOpen: onOpenHelp,
+    onClose: onCloseHelp,
+  } = useDisclosure();
 
-    // drawer state
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const handleHelp = () => {
+    onClose();
+    onOpenHelp();
+  };
 
-    // help drawer state
-    const { isOpen: isOpenHelp, onOpen: onOpenHelp, onClose: onCloseHelp } = useDisclosure()
+  return (
+    <SettingsPanelStyled>
+      <Button
+        className="button"
+        colorScheme="purple"
+        onClick={(e) => {
+          onOpen();
+          e.stopPropagation();
+        }}
+      >
+        Settings
+      </Button>
 
-    return (
-        <SettingsPanelStyled>
-            <Button
-                className="button" 
-                colorScheme='purple'
-                onClick={(e) => {
-                    onOpen()
-                    e.stopPropagation()
-                }}
-            >
-                Settings
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Settings</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Settings go here</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost" onClick={onOpenHelp}>
+              Help
             </Button>
 
-            <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Settings</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Text>Settings go here</Text>
-                    </ModalBody>
+            <Drawer
+              isOpen={isOpenHelp}
+              placement="bottom"
+              onClose={onCloseHelp}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Help</DrawerHeader>
+                <DrawerBody>
+                  <Text>Help goes here</Text>
+                </DrawerBody>
+                <DrawerFooter></DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </SettingsPanelStyled>
+  );
+};
 
-                    <ModalFooter>
-                        <Button colorScheme='purple' mr={3} onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button variant='ghost' onClick={onOpenHelp}>
-                            Help
-                        </Button>
-
-                        <Drawer isOpen={isOpenHelp} placement='bottom' onClose={onCloseHelp}>
-                            <DrawerOverlay />
-                            <DrawerContent>
-                                <DrawerCloseButton />
-                                <DrawerHeader>Help</DrawerHeader>
-
-                                <DrawerBody>
-                                    <Text>Help goes here</Text>
-                                </DrawerBody>
-                                <DrawerFooter>
-                                    <Button variant='ghost' onClick={onCloseHelp}>
-                                        Close
-                                    </Button>
-                                </DrawerFooter>
-                            </DrawerContent>
-                        </Drawer>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </SettingsPanelStyled>
-    )
-}
-
-export default SettingsPanel
+export default SettingsPanel;
