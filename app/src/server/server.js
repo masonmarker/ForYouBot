277f37a084bfb723244b3c996b4a0fa94f81bc0c
HarrisChaud <chaudhhx@dukes.jmu.edu>
2023-02-25 21:58:39 -0500

    Co-authored-by: masonmarker <masonmarker@users.noreply.github.com>

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -1,4 +1,22 @@
-console.log("Hello World");
-console.log("Hello World");
-
 const express = require("express");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const app = express();
+
+app.use(express.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.post("/ask", async (req, res) => {
+  try {
+    return res.status(200).json({ message: "Working" });
+  } catch (error) {}
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
