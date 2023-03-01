250a72bc160d18a526b28af602e135f12a6c6a46
masonmarker <markermd@dukes.jmu.edu>
2023-03-01 00:28:07 -0500

    Bot Update

--- app/src/components/Prompt.js
+++ app/src/components/Prompt.js
@@ -1,60 +1,49 @@
 /**
  * Input to the bot.
- * 
+ *
  * author :
  *  Mason Marker
  */
 
 // React
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 // for updating messages
-import { useMessages } from '../messages/messages'
+import { useMessages } from "../messages/messages";
 
 // Chakra components
-import {
-    Button,
-    Textarea,
-    Text,
-    HStack,
-    useColorMode
-} from "@chakra-ui/react"
+import { Button, Textarea, Text, HStack, useColorMode } from "@chakra-ui/react";
 
 // Chakra icons
-import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 // styled components
-import styled from 'styled-components'
-
-// OpenAI
-import ask from '../server/server'
+import styled from "styled-components";
 
 // max characters permitted in a single prompt
-const maxChars = 2000
+const maxChars = 2000;
 
 // styled Prompt
 const PromptStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    z-index: 1;
-         
-    .limtext {
-        position: fixed;
-        bottom: 0;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 80%;
-        margin-bottom: 3rem;
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 1;
 
-    .area {
-        z-index: 2;
-        height: 15vh;
-    }
+  .limtext {
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 80%;
+    margin-bottom: 3rem;
+  }
 
-`
-
+  .area {
+    z-index: 2;
+    height: 15vh;
+  }
+`;
 
 // Prompt component
 // should update messages array
@@ -66,94 +55,109 @@ const PromptStyled = styled.div`
 // or when the submit button is pressed should the request go through
 const Prompt = ({ messages, stateAddMessage }) => {
 
-    // get color mode
-    const { colorMode } = useColorMode()
+  // get color mode
+  const { colorMode } = useColorMode();
 
-    // handle enter press
-    // should not execute if shift is also being held
-    function handleEnterPress(e) {
-        if (e.key === "Enter" && !e.shiftKey) {
-            e.preventDefault()
-            document.getElementById("submit").click()
-        }
+  // handle enter press
+  // should not execute if shift is also being held
+  function handleEnterPress(e) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      document.getElementById("submit").click();
     }
+  }
 
-    // add message to messages
-    function addMessage(date, from, prompt) {
+  // add message to messages
+  async function addMessage(date, from, prompt) {
+    // if prompt exists, add it to messages
+    if (prompt.length > 0) {
+      // add message to messages
+      stateAddMessage({
+        date: date,
+        from: from,
+        message: prompt,
+      });
 
-        // if prompt exists, add it to messages
-        if (prompt.length > 0) {
+      // clear prompt
+      document.getElementsByClassName("area")[0].value = "";
 
-            // add message to messages
-            stateAddMessage({
-                date: date,
-                from: from,
-                message: prompt,
-            })
+      // reset character limit
+      document.getElementById("charlimit").innerHTML = "0/" + maxChars;
 
-            // clear prompt
-            document.getElementsByClassName("area")[0].value = ""
+      // reset character limit color
+      document.getElementById("charlimit").style.color =
+        colorMode === "light" ? "black" : "white";
 
-            // reset character limit
-            document.getElementById("charlimit").innerHTML = "0/" + maxChars
+      // make request to OpenAI
+      //  addMessage(new Date().toLocaleTimeString(), "bot", ask(prompt));
 
-            // reset character limit color
-            document.getElementById("charlimit").style.color = colorMode === "light" ? "black" : "white"
-        
-            // make request to OpenAI
-            addMessage(new Date().toLocaleTimeString(), "bot", ask(prompt))
-        }
+      // request to /ask on port 5000
+        const response = await fetch("http://localhost:5000/ask", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // allow CORS
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({ prompt: prompt }),
+        });
+        console.log(response);
     }
+  }
 
-    return (
-        <PromptStyled>
-            <HStack className="inp">
-                <Textarea colorScheme="purple"
-                    className="area"
-                    placeholder="Write a complex prompt..."
-                    maxLength={maxChars}
-                    onKeyPress={handleEnterPress}
-                    onChange={(e) => {
-                        const len = e.target.value.length
-                        document.getElementById("charlimit").innerHTML = len + `/${maxChars}`
+  return (
+    <PromptStyled>
+      <HStack className="inp">
+        <Textarea
+          colorScheme="purple"
+          className="area"
+          placeholder="Write a complex prompt..."
+          maxLength={maxChars}
+          onKeyPress={handleEnterPress}
+          onChange={(e) => {
+            const len = e.target.value.length;
+            document.getElementById("charlimit").innerHTML =
+              len + `/${maxChars}`;
 
-                        // character limit component
-                        const comp = document.getElementById("charlimit")
+            // character limit component
+            const comp = document.getElementById("charlimit");
 
-                        // if the length is greater than the max
-                        if (len >= maxChars) {
-                            comp.style.color = "red"
-                        }
-                        
-                        // if the color is red and the length is less than the max
-                        else if (comp.style.color === "red") {
-                            comp.style.color = colorMode === "light" ? "black" : "white"
-                        }
-                        else {
-                            comp.style.color = colorMode === "light" ? "black" : "white"
-                        }
+            // if the length is greater than the max
+            if (len >= maxChars) {
+              comp.style.color = "red";
+            }
 
-                    }}
-                />
-                <Button id="submit" onClick={() => {
-                    addMessage(
-                        new Date().toLocaleTimeString(),
-                        "user",
-                        document.getElementsByClassName("area")[0].value
-                    )
-                }}>
-                    <ArrowForwardIcon />
-                </Button>
-            </HStack>
+            // if the color is red and the length is less than the max
+            else if (comp.style.color === "red") {
+              comp.style.color = colorMode === "light" ? "black" : "white";
+            } else {
+              comp.style.color = colorMode === "light" ? "black" : "white";
+            }
+          }}
+        />
+        <Button
+          id="submit"
+          onClick={() => {
+            addMessage(
+              new Date().toLocaleTimeString(),
+              "user",
+              document.getElementsByClassName("area")[0].value
+            );
+          }}
+        >
+          <ArrowForwardIcon />
+        </Button>
+      </HStack>
 
-            <Text
-                color={colorMode === "light" ? "black" : "white"}
-                id="charlimit"
-                className="limtext"
-            >0/{maxChars}</Text>
-        </PromptStyled>
-    )
-}
+      <Text
+        color={colorMode === "light" ? "black" : "white"}
+        id="charlimit"
+        className="limtext"
+      >
+        0/{maxChars}
+      </Text>
+    </PromptStyled>
+  );
+};
 
-
-export default Prompt
+export default Prompt;
--- app/src/server/server.js
+++ app/src/server/server.js
@@ -1,9 +1,14 @@
-// const express = require("express");
+const express = require("express");
 // require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
-// const app = express();
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
+
+// use cors
+const cors = require("cors");
+app.use(cors());
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,44 +16,28 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-// app.post("/ask", async (req, res) => {
-//   try {
-//     const { prompt } = req.body;
-//     const response = await openai.complete({
-//         model: "text-davinci-003",
-//         prompt:  `
-//             ${prompt}
-//         `,
-//         max_tokens: 64,
-//         temperature: 0,
-//         top_p: 1.0,
-//         frequency_penalty: 0.0,
-//         presence_penalty: 0.0
-//     });
+app.post("/ask", async (req, res) => {
+    const { prompt } = req.body;
+    const response = await openai.complete({
+        model: "text-davinci-003",
+        prompt:  `
+            ${prompt}
+        `,
+        max_tokens: 64,
+        temperature: 0,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0
+    });
 
-//     return res.status(200).json({
-//         success: true,
-//         data: response.data.choices[0].text
-//     })
-//   } catch (error) {}
-// });
+    res.status(200).json({
+        success: true,
+        data: response.data.choices[0].text
+    })
+});
 
+// port
+const port = process.env.PORT || 5000;
 
-// sends a request to the API and returns the response
-export default async function ask(question) {
-  console.log("asking question...")
-  const response = await openai.createCompletion({
-    model: "text-ada-001",
-    prompt: question,
-    max_tokens: 64,
-    temperature: 0,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-  });
-  return response.data.choices[0].text;
-}
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server started on port ${port}`));
+// start listen
+app.listen(port, () => console.log(`Server started on port ${port}`));
