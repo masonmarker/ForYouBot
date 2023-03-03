f247fb1dc69f49bb62232a5c943e73f02bafbc85
masonmarker <markermd@dukes.jmu.edu>
2023-03-03 17:55:12 -0500

    Update

--- app/src/components/Convos.js
+++ app/src/components/Convos.js
@@ -10,22 +10,39 @@
 import {
     Box,
     Button,
-    Text
+    Text,
+
+    useColorMode
 } from "@chakra-ui/react"
 
 // styled components
 import styled from 'styled-components'
 
+// common
+import { colors } from '../common/common'
+
 // individual conversation styled
 const ConvoStyled = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 10%;
+    background-color: ${props => props.backgroundColor};
+    color: ${props => props.color};
+    
 
 `
 
 
 
-
 // Convos component
 const Convos = ({ conversations }) => {
+
+    // use color mode
+    const { colorMode } = useColorMode()
 
     // conversations format:
     // conversations[0].user[0] = {date: from: message:}
@@ -33,12 +50,16 @@ const Convos = ({ conversations }) => {
     return (
         <Box>
             <Text>Conversations</Text>
-            {conversations[0]['user'].length !== 0 ? conversations.map((convo, index) => {
+            {conversations[0].user.length !== 0 ? conversations.map((convo, index) => {
                 return (
-                    <Box key={`convo-${index}`}>
+                    <ConvoStyled
+                        key={`convo-${index}`}
+                        backgroundColor={colorMode === "light" ? "black" : "white"}
+                        color={colorMode === "light" ? "white" : "black"}
+                    >
                         <Text>{conversations[0].name}</Text>
                         <Text>{convo.user[0].message}</Text>
-                    </Box>
+                    </ConvoStyled>
 
                 )
             }) :
